1b8affeb61c3fd071c8ab641efa54e232feffb81
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-13 16:19:25 +0100

    controller cocktails ok & controller dose miss destroy

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
