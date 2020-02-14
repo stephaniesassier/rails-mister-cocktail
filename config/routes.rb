d7a4a69fee28f6f6f1a2d749205a39502c7fc071
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-14 17:46:21 +0100

    scss en transition

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy], shallow: true
   end
+
+  root to: 'cocktails#index'
 end
