3139ebbf501f52fb08134d4ad64957a47a98a56a
stephanie sassier <stephanie.sassier@gmail.com>
2020-04-23 10:21:19 +0200

    fix  bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,19 +90,17 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { :host => 'mister-cocktail-33.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'mister-cocktail-33.herokuapp.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true }
+config.action_mailer.default_url_options = { :host => 'mister-cocktail-33.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'mister-cocktail-33.herokuapp.com',
+  user_name: ENV["GMAIL_EMAIL"],
+  password: ENV["GMAIL_PASSWORD"],
+  authentication: 'plain',
+  enable_starttls_auto: true }
 end
